4b3d7079c9d7fa62ea17bc2e1fa6adbf6ca60fdf
Kshibata14 <113054100+Kshibata14@users.noreply.github.com>
2022-09-19 14:21:38 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     player.change(LedSpriteProperty.X, -1)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(game.score())
+})
 input.onButtonPressed(Button.B, function () {
     player.change(LedSpriteProperty.X, 1)
+})
+input.onGesture(Gesture.Shake, function () {
+    game.setScore(0)
+    basic.showNumber(game.score())
 })
 let player: game.LedSprite = null
 basic.showIcon(IconNames.Heart)
@@ -13,5 +20,6 @@ basic.forever(function () {
         enemy.delete()
         music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
         enemy = game.createSprite(randint(0, 5), 2)
+        game.addScore(1)
     }
 })
